365d7802d23741e288c22dfed8309dd8c1e8bec1
roniabusayeed <roni.abusayeed@gmail.com>
2022-08-10 20:07:52 +0600

    Implement Record component

---
+++ PasswordManagerLibrary/Record.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManagerLibrary
+{
+    public class Record : IRecord
+    {
+        private string _website;
+        private string _username;
+        private string _password;
+
+        /// <summary>
+        /// Initializes a new instance of the Record class with given website,
+        /// username, and password.
+        /// </summary>
+        /// <exception cref="InvalidWebsiteException"></exception>
+        /// <exception cref="InvalidUsernameException"></exception>
+        /// <exception cref="InvalidPasswordException"></exception>
+        public Record(string website, string username, string password)
+        {
+            if (! _setWebsite(website)) 
+            { 
+                throw new InvalidWebsiteException(
+                    "A website name cannot be empty or consists of only whitespaces.");
+            }
+            if (! SetUsername(username)) 
+            { 
+                throw new InvalidUsernameException(
+                    "A username cannot be empty, consists of only whitespaces, or have" +
+                    "whitespaces in between."); 
+            }
+            if (! SetPassword(password)) 
+            { 
+                throw new InvalidPasswordException(
+                    "A password cannot be empty."); 
+            }
+        }
+        private bool _setWebsite(string website)
+        {
+            // Leading and trailing whitespaces of a website name are ignored.
+            string trimmedWebsite = website.Trim();
+            
+            // A website name shouldn't contain only whitespaces.
+            if (trimmedWebsite.Length == 0) { return false; }
+
+            _website = trimmedWebsite;
+            return true;
+        }
+
+        public string GetPassword()
+        {
+            return _password;
+        }
+
+        public string GetUsername()
+        {
+            return _username;
+        }
+
+        public string GetWebsite()
+        {
+            return _website;
+        }
+
+        public bool SetPassword(string password)
+        {
+            // A password cannot be empty. (But it can contain only whitespaces)
+            if (password.Length == 0) { return false; }
+
+            _password = password;
+            return true;
+        }
+
+        public bool SetUsername(string username)
+        {
+            // Leading and trailing whitespaces of a username are ignored.
+            string trimmedUsername = username.Trim();
+
+            // A username cannot be empty or consists only of whitespaces.
+            if (trimmedUsername.Length == 0) { return false; }
+            
+
+            // A username cannot contain any whitespace character in between
+            // (i.e. besides the leading and the trailing ones).
+            if (trimmedUsername.Any(ch => char.IsWhiteSpace(ch)))   // if trimmedUsername
+                                                                    // has any whitespace in it
+            {
+                return false;
+            }
+            _username = trimmedUsername;
+            return true;
+        }
+    }
+}
---
+++ PasswordManagerLibraryTest/RecordTest.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PasswordManagerLibrary;
+
+namespace PasswordManagerLibraryTest
+{
+    [TestClass]
+    public class RecordTest     // Tests Record class.
+    {
+        [TestMethod]
+        public void Constructor_InvalidWebsite_Test()
+        {
+            // Arrange.
+            // Invalid website values.
+            string websiteEmtpy = string.Empty;
+            string websiteOnlyWhitespaces = " \t\n \t";
+
+            // Act and assert.
+            Assert.ThrowsException<InvalidWebsiteException>(() =>
+            {
+                IRecord record = new Record(websiteEmtpy, "username", "password");
+            });
+            Assert.ThrowsException<InvalidWebsiteException>(() =>
+            {
+                IRecord record = new Record(websiteOnlyWhitespaces, "username", "password");
+            });
+        }
+        [TestMethod]
+        public void Constructor_InvalidUsername_Test()
+        {
+            // Arrange.
+            // Invalid values.
+            string usernameEmpty = string.Empty;
+            string usernameOnlyWhitespaces = " \n\t   ";
+            string usernameWhitespacesInBetween = "user name";
+
+            // Act and assert.
+            Assert.ThrowsException<InvalidUsernameException>(() =>
+            {
+                IRecord record = new Record("website", usernameEmpty, "password");
+            });
+            Assert.ThrowsException<InvalidUsernameException>(() =>
+            {
+                IRecord record = new Record("website", usernameOnlyWhitespaces, "password");
+            });
+            Assert.ThrowsException<InvalidUsernameException>(() =>
+            {
+                IRecord record = new Record("website", usernameWhitespacesInBetween, "password");
+            });
+        }
+        [TestMethod]
+        public void Constructor_InvalidPassword_Test()
+        {
+            // Arrange.
+            string passwordEmpty = string.Empty;
+
+            // Act and assert.
+            Assert.ThrowsException<InvalidPasswordException>(() =>
+            {
+                IRecord record = new Record("website", "username", passwordEmpty);
+            });
+        }
+        [TestMethod]
+        public void Constructor_ValidWebsite_Test()
+        {
+            // Arrange.
+            // Valid websites.
+            string website = "website";
+            string websiteLeadingAndTrailingWhitespaces = "  website\t\t   ";
+            string websiteWhitespacesInBetween = "  website name\t\n  ";
+
+            // Act and assert.
+            // The following constructor calls shouldn't throw any exceptions.
+            IRecord? record = null;
+            record = new Record(website, "username", "password");
+            record = new Record(
+                websiteLeadingAndTrailingWhitespaces,
+                "username",
+                "password");
+            record = new Record(
+                websiteWhitespacesInBetween,
+                "username",
+                "password");
+        }
+        [TestMethod]
+        public void Constructor_ValidUsername_Test()
+        {
+            // Arrange.
+            string username = "username";
+            string usernameLeadingAndTrailingWhitespaces = "  \nusername\t  ";
+
+            IRecord? record = null;
+            record = new Record("website", username, "password");
+            record = new Record(
+                "website",
+                usernameLeadingAndTrailingWhitespaces,
+                "password");
+        }
+        [TestMethod]
+        public void Constructor_ValidPassword_Test()
+        {
+            // Arrange.
+            string password = "password";
+            string passwordOnlyWhitespaces = "  \t\n  ";
+
+            // Act and assert.
+            IRecord? record = null;
+            record = new Record("website", "username", password);
+            record = new Record("website", "username", passwordOnlyWhitespaces);
+        }
+        [TestMethod]
+        public void GetWebsite_Test()
+        {
+            // Arrange.
+            // Valid websites inputs and GetWebsite expected return values.
+            string website = "website";
+            string website_Expected = "website";
+
+            string websiteLeadingAndTrailingWhitespaces = "  website\t\t   ";
+            string websiteLeadingAndTrailingWhitespaces_Expected = "website";
+            
+            string websiteWhitespacesInBetween = "  website name\t\n  ";
+            string websiteWhitespacesInBetween_Expected = "website name";
+
+            // Act and assert.
+            Assert.AreEqual(
+                new Record(website, "username", "password").GetWebsite(),
+                website_Expected);
+            Assert.AreEqual(
+                new Record(websiteLeadingAndTrailingWhitespaces, "username", "password")
+                .GetWebsite(),
+                websiteLeadingAndTrailingWhitespaces_Expected);
+            Assert.AreEqual(
+                new Record(websiteWhitespacesInBetween, "username", "password").GetWebsite(),
+                websiteWhitespacesInBetween_Expected);
+        }
+        [TestMethod]
+        public void GetUsername_Test()
+        {
+            // Arrange.
+            // Valid username inputs and GetUsername expected return values.
+            string username = "username";
+            string username_Expected = "username";
+
+            string usernameLeadingAndTrailingWhitespaces = "  \nusername\t  ";
+            string usernameLeadingAndTrailingWhitespaces_Expected = "username";
+
+            Assert.AreEqual(
+                new Record("website", username, "password").GetUsername(),
+                username_Expected);
+            Assert.AreEqual(
+                new Record("website", usernameLeadingAndTrailingWhitespaces, "password").GetUsername(),
+                usernameLeadingAndTrailingWhitespaces_Expected);
+        }
+        [TestMethod]
+        public void GetPassword_Test()
+        {
+            // Arrange.
+            // Valid password inputs and GetPassword expected return values.
+            string password = "password";
+            string password_Expected = password;
+
+            string passwordOnlyWhitespaces = "  \t\n  ";
+            string passwordOnlyWhitespaces_Expected = passwordOnlyWhitespaces;
+
+            // Act and assert.
+            Assert.AreEqual(
+                new Record("website", "username", password).GetPassword(),
+                password_Expected);
+            Assert.AreEqual(
+                new Record("website", "username", passwordOnlyWhitespaces).GetPassword(),
+                passwordOnlyWhitespaces_Expected);
+        }
+    }
+}
