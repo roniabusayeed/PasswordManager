2b673176bd78a4c55fc6342c8cf9d7c217bd6c26
roniabusayeed <roni.abusayeed@gmail.com>
2022-08-11 12:50:35 +0600

    Add a record manager with add and find feature

---
+++ PasswordManagerLibrary/RecordManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManagerLibrary
+{
+    public class RecordManager
+    {
+        private readonly Dictionary<string, IRecord> _records;
+
+        /// <summary>
+        /// Creates a new instance of the RecordManager class.
+        /// </summary>
+        public RecordManager()
+        {
+            _records = new Dictionary<string, IRecord>();
+        }
+
+        /// <summary>
+        /// Finds the record for a given a website.
+        /// </summary>
+        /// <param name="website">A string containing the name of the website</param>
+        /// <returns>A reference to the Record instance if it is found. Otherwise
+        /// returns null.</returns>
+        public IRecord? FindRecord(string website)
+        {
+            try
+            {
+                return _records[website];
+            } catch (KeyNotFoundException)
+            {
+                return null;
+            }          
+        }
+
+        /// <summary>
+        /// Adds a new record to this RecordManager instance. If there is
+        /// already a record with identical Website, it updates the record.
+        /// </summary>
+        /// <param name="record">Reference to the record instance.</param>
+        /// <returns>true if the record is added successfully. Otherwise,
+        /// returns false.</returns>
+        public bool AddRecord(IRecord record)
+        {
+            _records[record.GetWebsite()] = record;
+            return true;
+        }
+    }
+}
---
+++ PasswordManagerLibraryTest/RecordManagerTest.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PasswordManagerLibrary;
+
+namespace PasswordManagerLibraryTest
+{
+    [TestClass]
+    public class RecordManagerTest
+    {
+        [TestMethod]
+        public void FindRecord_Empty_Test()
+        {
+            // Arrange.
+            RecordManager manager = new();
+
+            //Act.
+            IRecord? result = manager.FindRecord("website");
+
+            // Assert.
+            Assert.IsNull(result);
+        }
+        [TestMethod]
+        public void FindRecord_NonEmpty_Test_1()
+        {
+            // Arrange.
+            RecordManager manager = new();
+            IRecord record = new Record("website", "username", "password");
+            manager.AddRecord(record);
+
+            // Act.
+            IRecord? foundRecord = manager.FindRecord("website");
+
+            // Assert.
+            Assert.IsNotNull(foundRecord);
+            Assert.AreEqual(foundRecord, record);
+        }
+        [TestMethod]
+        public void FindRecord_NonEmpty_Test2()
+        {
+            // Arrange.
+            RecordManager manager = new();
+            IRecord record = new Record("website", "username", "password");
+            manager.AddRecord(record);
+
+            // Act.
+            IRecord? foundRecord = manager.FindRecord("non-existent website");
+
+            // Assert.
+            Assert.IsNull(foundRecord);
+        }
+        [TestMethod]
+        public void AddRecord_Empty_Test()
+        {
+            // Arrange.
+            RecordManager manager = new();
+            IRecord record = new Record("website", "username", "password");
+
+            // Act.
+            bool result = manager.AddRecord(record);
+
+            // Assert.
+            Assert.IsTrue(result);
+            IRecord? foundRecord = manager.FindRecord(record.GetWebsite());
+            Assert.IsNotNull(foundRecord);
+            Assert.AreEqual(foundRecord, record);
+        }
+        [TestMethod]
+        public void AddRecord_NonEmpty_Test_1()
+        {
+            // Arrange.
+            RecordManager manager = new();
+            IRecord record = new Record("website", "username", "password");
+            manager.AddRecord(record);
+            IRecord record2 = new Record("website2", "username2", "password2");
+
+            // Act and assert.
+            bool result = manager.AddRecord(record2);
+            Assert.IsTrue(result);
+            IRecord? foundRecord = manager.FindRecord(record2.GetWebsite());
+            Assert.IsNotNull(foundRecord);
+            Assert.AreEqual(foundRecord, record2);
+        }
+        [TestMethod]
+        public void AddRecord_NonEmpty_Test_2()
+        {
+            // Arrange.
+            RecordManager manager = new();
+            const string website = "website";
+            const string modified_username = "modified-username";
+            const string modified_password = "modified-password";
+            IRecord record = new Record(website, "username", "password");
+            manager.AddRecord(record);
+            IRecord record2 = new Record(website, modified_username, modified_password);
+
+            // Act.
+            manager.AddRecord(record2);
+
+            // Assert.
+            IRecord? foundRecord = manager.FindRecord(website);
+            Assert.IsNotNull(foundRecord);
+            Assert.AreEqual(foundRecord.GetUsername(), modified_username);
+            Assert.AreEqual(foundRecord.GetPassword(), modified_password);
+        }
+    }
+}
